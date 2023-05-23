e47dfbb590004f065d7d0c7543b90e7425226a1e
thomasshelby3 <134419997+thomasshelby3@users.noreply.github.com>
2023-05-23 20:28:16 +0300

    Update and rename README.md to MusicPlayer.java

---
+++ MusicPlayer.java
@@ -0,0 +1,30 @@
+// Song.java
+public class Song {
+    private String title;
+    private String artist;
+    private String album;
+    private int duration;
+
+    public Song(String title, String artist, String album, int duration) {
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# MusicPlayer
-Users can browse their music library, create playlists, play/pause/seek songs, and adjust the volume. The app provides features such as audio file format support, playlist management, and a user-friendly interface. 
